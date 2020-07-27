ea6339e5a95ca13d4f149a1a305ebd0d6d2ceb2f
jpriver49 <jprivera49@hotmail.com>
2020-07-27 00:17:44 -0700

    Add scrape app

--- scraping.py
+++ scraping.py
@@ -1,22 +1,28 @@
-# Import Splinter and Beautiful Soup
+# Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup 
 import pandas as pd
 import datetime as dt
-
+   
 def scrape_all():
     # Initiate headless driver for deployment
-    browser = Browser("chrome", executable_path = "/Users/jpriv/Anaconda3/Scripts/chromedriver.exe", headless=True)
+    executable_path={'executable_path': '/Users/jpriv/Anaconda3/Scripts/chromedriver.exe'}
+    browser = Browser("chrome", **executable_path, headless=True)
 
-    news_title, news_paragraph = mars_news(browser)
-
+    news_title, news_p = mars_news(browser)
+    img_url = featured_image(browser)
+    facts = mars_facts()
+    hemisphere_image_urls = hemisphere(browser)
+    timestamp = dt.datetime.now()
+    
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "news_paragraph": news_p,
+        "featured_image": img_url,
+        "facts": facts,
+        "hemispheres": hemisphere_image_urls,
+        "last_modified": timestamp
     }
 
     # Stop webdriver and return data
@@ -24,22 +30,27 @@ def scrape_all():
     return data
 
 def mars_news(browser):
+
+    # Scrape Mars News
     # Visit the mars nasa news site
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
+
     # Optional delay for loading the page
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
+    # Convert the browser html to a soup object and then quit the browser
     html = browser.html
     news_soup = BeautifulSoup(html, 'html.parser')
 
+    # Add try/except for error handling
     try:
-        slide_elem = news_soup.select_one('ul.item_list li.slide')
-        # Use the parent element to find the first `a` tag and save it as `news_title`
-        news_title = slide_elem.find("div", class_='content_title').get_text()
+        slide_elem = news_soup.select_one("ul.item_list li.slide")
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find("div", class_="content_title").get_text()
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
-    
+        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
+
     except AttributeError:
         return None, None
 
@@ -51,7 +62,7 @@ def featured_image(browser):
     browser.visit(url)
 
     # Find and click the full image button
-    full_image_elem = browser.find_by_id('full_image')
+    full_image_elem = browser.find_by_id('full_image')[0]
     full_image_elem.click()
 
     # Find the more info button and click that
@@ -67,30 +78,74 @@ def featured_image(browser):
     try:
         # Find the relative image url
         img_url_rel = img_soup.select_one('figure.lede a img').get("src")
-    
+
     except AttributeError:
         return None
 
-    # Use the base URL to create an absolute URL
+    # Use the base url to create an absolute url
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-        
+
     return img_url
 
 def mars_facts():
+    # Add try/except for error handling
     try:
-        # use 'read html' to scrape the facts tabe into a dataframe
+        # Use 'read_html' to scrape the facts table into a dataframe
         df = pd.read_html('http://space-facts.com/mars/')[0]
+
     except BaseException:
         return None
-    
-    # Assign columns and set index of dataframe
-    df.columns=['description', 'value']
-    df.set_index('description', inplace=True)
 
-    # Convert dataFrame into HTML format, add bootstrap
-    return df.to_html()
+    # Assign columns and set index of dataframe
+    df.columns=['Description', 'Value']
+    df.set_index('Description', inplace=True)
+
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html(classes="table table-striped")
+
+def hemisphere(browser):
+    # Visit hemispheres website through splinter module 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+    html_hemispheres = browser.html
+    hemisphere_soup = BeautifulSoup(html_hemispheres, 'html.parser')
+
+    # Retreive all items that contain mars hemispheres information
+    items = hemisphere_soup.find_all('div', class_='item')
+
+    # Create empty list for hemisphere urls 
+    hemisphere_image_urls = []
+
+    # Store the main_ul 
+    hemispheres_main_url = 'https://astrogeology.usgs.gov' 
+
+    # Loop through the items previously stored
+    for i in items: 
+        # Store title
+        title = i.find('h3').text
+            
+        # Store link that leads to full image website
+        img_url = i.find('a', class_='itemLink product-item')['href']
+            
+        # Visit the link that contains the full image website 
+        browser.visit(hemispheres_main_url + img_url)
+            
+        # HTML Object of individual hemisphere information website 
+        img_html = browser.html
+            
+        # Parse HTML with Beautiful Soup for every individual hemisphere information website 
+        hemisphere_soup = BeautifulSoup(img_html, 'html.parser')
+            
+        # Retrieve full image source 
+        img_url = hemispheres_main_url + hemisphere_soup.find('img', class_='wide-image')['src']
+            
+        # Append the retreived information into a list of dictionaries 
+        hemisphere_image_urls.append({"title" : title, "img_url" : img_url})
+    
+    browser.quit()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
-
-    # If runnin as script, print scraped data
+    # If running as script, print scraped data
     print(scrape_all())
